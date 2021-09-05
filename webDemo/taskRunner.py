09558675b778ea7c41ff14c1e3ec25084c6af642
984355579@qq.com <984355579@qq.com>
2021-09-05 20:23:40 +0800

    提交更新

--- webDemo/taskRunner.py
+++ webDemo/taskRunner.py
@@ -3,4 +3,4 @@ import os
 import pytest
 if __name__ == '__main__':
     pytest.main()
-    os.system("allure generate ./temp -o ./reports --clean")
+    # os.system("allure generate ./temp -o ./reports --clean")
